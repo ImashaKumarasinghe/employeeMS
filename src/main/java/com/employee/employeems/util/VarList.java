3cdd4c3d5248d9bd1173bed4550a1c767fc45d97
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-26 16:04:19 +0530

    add model mapper and create VarList.java

---
+++ src/main/java/com/employee/employeems/util/VarList.java
@@ -0,0 +1,10 @@
+package com.employee.employeems.util;
+
+public class VarList {
+    // Common Status Codes
+    public static final String SUCCESS = "00";
+    public static final String ERROR = "01";
+    public static final String NOT_FOUND = "02";
+    public static final String DUPLICATE = "03";
+
+}
