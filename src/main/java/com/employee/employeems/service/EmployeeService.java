c05480d122309a9b5ed0763b1687817b147864bf
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-27 23:20:13 +0530

    create method for searchEmployees in service layer

--- src/main/java/com/employee/employeems/service/EmployeeService.java
+++ src/main/java/com/employee/employeems/service/EmployeeService.java
@@ -15,46 +15,50 @@ import java.util.List;
 
 @Service
 @Transactional
-
 public class EmployeeService {
 
     @Autowired
     private EmployeeRepo employeeRepo;
+
     @Autowired
     private ModelMapper modelMapper;
 
-//DTOs are used in the service layer to transfer data
-// safely between layers without exposing the database entity.
+    //DTOs are used in the service layer to transfer data
+    // safely between layers without exposing the database entity.
     //EmployeeDTO is the data type (class),
-// and employeeDTO is the variable that holds the object passed to the method.
-    public String saveEmployee(EmployeeDTO employeeDTO){
-        if(employeeDTO.getEmpId() != null && employeeRepo.existsById(employeeDTO.getEmpId())){
+    // and employeeDTO is the variable that holds the object passed to the method.
+    public String saveEmployee(EmployeeDTO employeeDTO) {
+        if (employeeDTO.getEmpId() != null && employeeRepo.existsById(employeeDTO.getEmpId())) {
             return VarList.DUPLICATE;
-        }
-
-    else {
-        employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
-        return VarList.SUCCESS;
-        }
-
-
-
-
-}
-    public String updateEmployee(EmployeeDTO employeeDTO){
-        if (employeeDTO.getEmpId() != null && employeeRepo.existsById(employeeDTO.getEmpId())){
-            employeeRepo.save(modelMapper.map(employeeDTO,Employee.class));
+        } else {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
             return VarList.SUCCESS;
+        }
+    }
 
-        }else {
+    public String updateEmployee(EmployeeDTO employeeDTO) {
+        if (employeeDTO.getEmpId() != null && employeeRepo.existsById(employeeDTO.getEmpId())) {
+            employeeRepo.save(modelMapper.map(employeeDTO, Employee.class));
+            return VarList.SUCCESS;
+        } else {
             return VarList.NOT_FOUND;
         }
-
     }
-    public String getAllEmployees(){
-        List<Employee> employeeList= employeeRepo.findAll();
-        return modelMapper.map(employeeList,new TypeToken<ArrayList<EmployeeDTO>>(){
 
-        }.getType());
+    public List<EmployeeDTO> getAllEmployees() {
+        List<Employee> employeeList = employeeRepo.findAll();
+        return modelMapper.map(employeeList,
+                new TypeToken<ArrayList<EmployeeDTO>>() {
+                }.getType());
     }
+
+    public EmployeeDTO searchEmployee(Integer empId) {
+        if (employeeRepo.existsById(empId)) {
+            Employee employee = employeeRepo.findById(empId).get();
+            return modelMapper.map(employee, EmployeeDTO.class);
+        } else {
+            return null;
+        }
+    }
+
 }
