6317c661fe5d949766ec66a2f9a8723b8ec691cc
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-27 22:28:59 +0530

    create updateEmployee function in controller layer

--- src/main/java/com/employee/employeems/controller/EmployeeController.java
+++ src/main/java/com/employee/employeems/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.employee.employeems.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/employee")
@@ -58,5 +55,42 @@ public ResponseEntity saveEmployee(@RequestBody EmployeeDTO employeeDTO){
         return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+    @PutMapping(value = "/updateemployee")
+    //ResponseEntity is used to return the HTTP response with full control over the
+    // response body, status code, and headers.
+
+
+    public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+
+        try {
+            String res=employeeService.updateEmployee(employeeDTO);
+            if(res.equals("00")){
+                responseDTO.setCode(VarList.SUCCESS);
+                responseDTO.setMessage("success");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+            } else if (res.equals("02")) {
+                responseDTO.setCode(VarList.NOT_FOUND);
+                responseDTO.setMessage("Not Registered employee");
+                responseDTO.setContent(employeeDTO);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            }else{
+                responseDTO.setCode(VarList.ERROR);
+                responseDTO.setMessage("Fail");
+                responseDTO.setContent(null);
+                return new ResponseEntity(responseDTO, HttpStatus.BAD_REQUEST);
+
+            }
+
+        } catch (Exception ex) {
+            responseDTO.setCode(VarList.ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 
 }
